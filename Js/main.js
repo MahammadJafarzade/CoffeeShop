ac4455641377197c77bc9a93c1d4ac2d11374a21
MahammedJafarzade <7a4m9bb@code.edu.az>
2022-07-31 17:31:06 +0400

    Coffee

--- Js/main.js
+++ Js/main.js
@@ -105,4 +105,26 @@ $('.media-info').slick({
     // instead of a settings object
   ]
 });
-
+function getVals(){
+  let parent=this.parentNode;
+  let slides=parent.getElementsByTagName("input");
+  let slide1 = parseFloat(slides[0].value);
+  let slide2 = parseFloat(slides[1].value);
+  if(slide1>slide2){
+    let tmp=slide2; slide2=slide1; slide1=tmp;
+  }
+  let displayElement=parent.getElementsByClassName("range-values")[0];
+  displayElement.innerHTML="$" + slide1 + "-$" + slide2;
+}
+window.onload=function(){
+  let sliderSections=document.getElementsByClassName("range-slider");
+  for(let x = 0; x<sliderSections.length; x++){
+    let sliders = sliderSections[x].getElementsByTagName("input");
+    for(let y = 0; y<sliderSections.length; y++){
+      if(sliders[y].type === "range"){
+        sliders[y].oninput=getVals;
+        sliders[y].oninput();
+      }
+    }
+  }
+}

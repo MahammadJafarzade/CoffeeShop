efa4bea1ca1d07d03683d566915f2dbfa244aac4
MahammedJafarzade <7a4m9bb@code.edu.az>
2022-08-06 16:31:20 +0400

    Coffee

--- CoffeeShop/CoffeeShop/Startup.cs
+++ CoffeeShop/CoffeeShop/Startup.cs
@@ -12,6 +12,7 @@ namespace CoffeeShop
 {
     public class Startup
     {
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,14 +28,15 @@ namespace CoffeeShop
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{Id?}"
+                    );
             });
         }
     }
